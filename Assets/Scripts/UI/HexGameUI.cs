7564ef01feba689e71344791dd119f06b4c2590b
Casper van Battum <cvanbattum@gmail.com>
2020-06-11 19:23:19 +0200

    Part 21.1

--- Assets/Scripts/UI/HexGameUI.cs
+++ Assets/Scripts/UI/HexGameUI.cs
@@ -12,6 +12,12 @@ public class HexGameUI : MonoBehaviour {
         enabled = !toggle;
         grid.ShowUI(!toggle);
         grid.ClearPath();
+        if (toggle) {
+            Shader.EnableKeyword("HEX_MAP_EDIT_MODE");
+        }
+        else {
+            Shader.DisableKeyword("HEX_MAP_EDIT_MODE");
+        }
     }
 
     private void Update() {
