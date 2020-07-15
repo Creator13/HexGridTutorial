d6a1795df6c03b6e22ca59f06fc609e59a9a085e
Casper van Battum <cvanbattum@gmail.com>
2020-07-15 16:37:42 +0200

    part 23.2

--- Assets/Scripts/HexMapGenerator.cs
+++ Assets/Scripts/HexMapGenerator.cs
@@ -1,12 +1,78 @@
 ﻿using UnityEngine;
 
 public class HexMapGenerator : MonoBehaviour {
+    [SerializeField, Range(0, .5f)] private float jitterProbability = .25f;
+    [SerializeField, Range(20,200)] private int chunkSizeMin = 30;
+    [SerializeField, Range(20,200)] private int chunkSizeMax = 100;
+    [SerializeField, Range(0, 100)] private int landPercentage = 50;
+    
     public HexGrid grid;
 
+    private int cellCount;
+    private PriorityQueue<HexCell> searchFrontier;
+    private int searchFrontierPhase;
+
     public void GenerateMap(int x, int z) {
-        grid.CreateMap(x, z);
-        for (var i = 0; i < z; i++) {
-            grid.GetCell(x / 2, i).TerrainTypeIndex = 1;
+        cellCount = x * z;
+
+        if (searchFrontier == null) {
+            searchFrontier = new PriorityQueue<HexCell>();
         }
+
+        grid.CreateMap(x, z);
+
+        CreateLand();
+        
+        for (var i = 0; i < cellCount; i++) {
+            grid.GetCell(i).SearchPhase = 0;
+        }
+    }
+
+    private void CreateLand() {
+        var landBudget = Mathf.RoundToInt(cellCount * landPercentage * .01f);
+
+        while (landBudget > 0) {
+            landBudget = RaiseTerrain(Random.Range(chunkSizeMin, chunkSizeMax + 1), landBudget);
+        }
+    }
+
+    private int RaiseTerrain(int chunkSize, int budget) {
+        searchFrontierPhase++;
+        var firstCell = GetRandomCell();
+        firstCell.SearchPhase = searchFrontierPhase;
+        firstCell.Distance = 0;
+        firstCell.SearchHeuristic = 0;
+        searchFrontier.Enqueue(firstCell);
+        var center = firstCell.coordinates;
+
+        var size = 0;
+        while (size < chunkSize && searchFrontier.Count > 0) {
+            var current = searchFrontier.Dequeue();
+            if (current.TerrainTypeIndex == 0) {
+                current.TerrainTypeIndex = 1;
+                if (--budget == 0) {
+                    break;
+                }
+            }
+
+            size += 1;
+
+            for (var d = HexDirection.NE; d <= HexDirection.NW; d++) {
+                var neighbor = current.GetNeighbor(d);
+                if (neighbor && neighbor.SearchPhase < searchFrontierPhase) {
+                    neighbor.SearchPhase = searchFrontierPhase;
+                    neighbor.Distance = neighbor.coordinates.DistanceTo(center);
+                    neighbor.SearchHeuristic = Random.value < jitterProbability ? 1 : 0;
+                    searchFrontier.Enqueue(neighbor);
+                }
+            }
+        }
+
+        searchFrontier.Clear();
+        return budget;
+    }
+
+    private HexCell GetRandomCell() {
+        return grid.GetCell(Random.Range(0, cellCount));
     }
 }
